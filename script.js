86b773c63aaee15ac3a2a02dd9d09125b09c402d
Sonu <100422918+uniquesonu@users.noreply.github.com>
2023-02-27 02:53:59 +0530

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+const cards = document.querySelector('.cards')
+
+const populate = async(e) => {
+    const data = await fetch('./data.json');
+    const response = await data.json();
+    console.log(response)
+    cards.innerHTML = response.map(card =>{
+        return `
+        <div class="card-wrapper">
+        <div class="icon"><img src="${card.icon}"></div>
+        <div class="name">${card.category}</div>
+        <p class="score">${card.score}&nbsp;<span>/ 100</span></p>
+    </div>
+        `
+    }).join('')
+
+
+}
+populate();
